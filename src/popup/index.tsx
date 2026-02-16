900d6f2271ad442a2b5d182f014bdcc2414cfacc
a-irch <larocatonio@gmail.com>
2026-02-16 19:03:40 +0100

    initial setup

---
+++ src/popup/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import "~/globals.css"
+
+const IndexPopup = () => {
+  return (
+    <div className="flex h-[5rem] w-[10rem] flex-col gap-3">
+      <h1 className="text-xl items-center">RotGuard</h1>
+      <Button className="w-2/3">Test</Button>
+    </div>
+  )
+}
+
+export default IndexPopup
