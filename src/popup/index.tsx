a742e9ea1c7020a9a9d463f3df9c48cb596ee90a
a-irch <larocatonio@gmail.com>
2026-02-23 12:12:09 +0100

    feat : logo and options in popup

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,12 +1,29 @@
 import { Button } from '@/components/ui/button';
+import logo from 'data-base64:~../assets/icon512.png';
 
 import '@/globals.css';
 
+import { Cog } from 'lucide-react';
+
 const IndexPopup = () => {
+  const openOptions = () => {
+    chrome.runtime.openOptionsPage();
+  };
+
   return (
-    <div className="flex h-[5rem] w-[10rem] flex-col gap-3">
-      <h1 className="items-center text-xl">RotGuard</h1>
-      <Button className="w-2/3">Test</Button>
+    <div className="flex w-[16rem] items-center justify-between p-4">
+      <div className="flex items-center gap-2">
+        <img
+          src={logo}
+          alt="RotGuard Logo"
+          className="h-8 w-8 object-contain"
+        />
+        <h1 className="text-xl font-bold">RotGuard</h1>
+      </div>
+
+      <Button variant="ghost" size="icon" onClick={openOptions}>
+        <Cog className="h-6 w-6" />
+      </Button>
     </div>
   );
 };
