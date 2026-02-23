9bb20b1ca2e29b3e5eac7f07a8ca7f3b8cbce131
a-irch <larocatonio@gmail.com>
2026-02-23 17:18:34 +0100

    add : footer

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -9,13 +9,14 @@ import {
 import '@/globals.css';
 
 import { Toaster } from '@/components/ui/sonner';
+import { Github, Globe } from 'lucide-react';
 
 import RestrictList from './RestrictList';
 import SessionTime from './SessionTime';
 
 const Options = () => {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       <div className="mx-auto flex w-[52rem] flex-col gap-4 py-6">
         <Card>
           <CardHeader>
@@ -31,8 +32,29 @@ const Options = () => {
           </CardContent>
         </Card>
       </div>
+      <footer className="mx-8 my-6 mt-auto flex justify-between gap-3 pt-8 text-sm text-muted-foreground">
+        <p>RotGuard • Created by a-irch</p>
+
+        <div className="flex gap-6">
+          <a
+            href="https://github.com/a-irch/RotGuard"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-1.5 transition-colors hover:text-foreground">
+            <Github className="h-4 w-4" />
+          </a>
+
+          <a
+            href="https://a-irch.fr/en"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-1.5 transition-colors hover:text-foreground">
+            <Globe className="h-4 w-4" />
+          </a>
+        </div>
+      </footer>
       <Toaster />
-    </>
+    </div>
   );
 };
 
